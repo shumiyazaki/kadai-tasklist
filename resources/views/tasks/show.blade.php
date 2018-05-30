f17324e999079674d4dd32a5ce64369243c21bef
EC2 Default User <ec2-user@ip-172-31-45-248.us-east-2.compute.internal>
2018-05-30 07:59:47 +0000

    fixed error

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,9 +4,12 @@
 
     <h1>id = {{ $task->id }} のタスクの詳細ページ</h1>
 
+    <p>ステータス: {{ $task->status }}</p>
+    <p>タスク: {{ $task->content }}</p>
+
     <p>{{ $task->content }}</p>
     
-        {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id]) !!}
+        
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
